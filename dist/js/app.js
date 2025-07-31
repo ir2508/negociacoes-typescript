fbb83cc1c6cb2a4d47d32e70252ca192092558ee
Igor Roberto <roberto.igor.18@gmail.com>
2025-07-31 02:02:48 -0300

    Instalacao do typescript no projeto e configuracao do tsconfig

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,6 @@
 import { Negociacao } from "./models/negociacao.js";
-
-const negociacao = new Negociacao(new Date(), 10, 100)
-console.log(negociacao)
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao);
 // negociacao.quantidade = 10000
-console.log(negociacao.volume)
+console.log(negociacao.volume);
+alert("oi");
